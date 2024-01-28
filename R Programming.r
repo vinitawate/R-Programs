5545e92e9e15f7dc8dfe27402d11912745982de4
vinitawate <146056800+vinitawate@users.noreply.github.com>
2024-01-28 22:31:12 +0530

    Add files via upload

---
+++ R Programming.r
@@ -0,0 +1,58 @@
+#VARIABLES AND CONSTANT
+
+#Boolean variables
+a=TRUE
+print(a)
+print(class(a))
+
+#integer variables
+A=14L
+print(A)
+print(class(A))
+
+#floating point variables
+x=13.4
+print(x)
+print(class(x))
+
+#character variables
+alphabhet="A"
+print(alphabhet)
+print(class(alphabhet))
+
+#string variables
+message="Welcome to India"
+print(message)
+print((class(message)))
+
+
+
+
+#changing value of variable
+message="HELLO WORLD"
+print(message)
+message<-"VinZie"
+print(message)
+
+#integer constant
+x<-15L
+print(typeof(x))
+print(class(x))
+
+#logical constant
+
+x<-TRUE
+y<-FALSE
+print(x)
+print(y)
+
+#Built in r constant
+
+#print list of uppercase letters
+print(LETTERS)
+#print list of lowercase letters
+print(letters)
+#print 3 letters abbreviation of english months 
+print(month.abb)
+#print numerical value of constamt pi
+print(3.14)
